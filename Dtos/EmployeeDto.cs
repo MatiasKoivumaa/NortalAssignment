2a2fcc7a0a7a2e0f4f4f338db0275e32efc39883
Matias Koivumaa <matiaskoivumaa@gmail.com>
2023-01-11 16:54:25 +0200

    DTOs

---
+++ Dtos/EmployeeDto.cs
@@ -0,0 +1,13 @@
+﻿using AssignmentApi.Models;
+
+namespace AssignmentApi
+{
+    public class EmployeeDto
+    {
+        public int Id { get; init; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string JobTitle { get; set; } = string.Empty;
+        public List<ProjectViewModel> Projects { get; set; } = new List<ProjectViewModel>();
+    }
+}
---
+++ Dtos/ProjectDto.cs
@@ -0,0 +1,11 @@
+﻿using AssignmentApi.Models;
+
+namespace AssignmentApi.Dtos
+{
+    public class ProjectDto
+    {
+        public int Id { get; init; }
+        public string ProjectName { get; set; } = string.Empty;
+        public List<EmployeeViewModel> Employees { get; set; } = new List<EmployeeViewModel>();
+    }
+}
