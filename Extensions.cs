385598aa0ad4930994427f05535ed99576be95cb
Matias Koivumaa <matiaskoivumaa@gmail.com>
2023-01-11 16:52:38 +0200

    init create project

---
+++ Extensions.cs
@@ -0,0 +1,50 @@
+﻿using AssignmentApi.Models;
+using AssignmentApi.Dtos;
+
+namespace AssignmentApi
+{
+    public static class Extensions
+    {
+        public static EmployeeDto EmployeeAsDto(this Employee employee)
+        {
+            return new EmployeeDto
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                JobTitle = employee.JobTitle,
+                Projects = employee.Projects.Select(p => p.ProjectAsViewModel()).ToList()
+            };
+        }
+
+        public static ProjectDto ProjectAsDto(this Project project)
+        {
+            return new ProjectDto
+            {
+                Id = project.Id,
+                ProjectName = project.ProjectName,
+                Employees = project.Employees.Select(e => e.EmployeeAsViewModel()).ToList()
+            };
+        }
+
+        public static ProjectViewModel ProjectAsViewModel(this Project project)
+        {
+            return new ProjectViewModel
+            {
+                Id = project.Id,
+                ProjectName = project.ProjectName
+            };
+        }
+
+        public static EmployeeViewModel EmployeeAsViewModel(this Employee employee)
+        {
+            return new EmployeeViewModel
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                JobTitle = employee.JobTitle
+            };
+        }
+    }
+}
